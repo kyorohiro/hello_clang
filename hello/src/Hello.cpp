6e51d252000adedb2496a12671fefa3838237694
kyorohiro <kyorohiro@gmail.com>
2019-05-02 15:12:14 +0000

    fc

---
+++ hello/src/Hello.cpp
@@ -0,0 +1,6 @@
+#include "Hello.h"
+#include <iostream>
+
+void Hello::print() {
+    std::cout <<  "Hello!!";
+}
---
+++ hello/src/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "Hello.h"
+
+int main(int argc, char** argv) {
+    Hello hello;
+    hello.print();
+    return 0;
+}
